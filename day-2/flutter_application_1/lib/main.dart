dbe4d16f3388a61d86b2c16dd0a3d35234af4fef
Ripat-2345 <ripatalsafar@gmail.com>
2021-08-06 10:53:30 +0700

    import assets

---
+++ day-2/flutter_application_1/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Day2());
+}
+
+class Day2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
